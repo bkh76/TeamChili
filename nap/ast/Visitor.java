ca3610a3d699f35750e6bed7ed572fb185a51b97
bkh76 <55768460+bkh76@users.noreply.github.com>
2020-02-26 14:37:09 -0700

    Update to Visitor.java
    Added new expression operation classes

--- nap/ast/Visitor.java
+++ nap/ast/Visitor.java
@@ -2,9 +2,13 @@ package ast;
 
 public interface Visitor<T> {
     public T visit(ExpBinop expression);
+    public T visit(ExpEqualityop expression);
+    public T visit(ExpLogicalop expression);
+    public T visit(ExpNegop expression);
+    public T visit(ExpRelationop expression);
     public T visit(ExpInt number);
     public T visit(ExpVar variable);
-    public T visit(Write instruction);
+    public T visit(Print instruction);
     public T visit(Assign instruction);
     public T visit(Program program);
 }
