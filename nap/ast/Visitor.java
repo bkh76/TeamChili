9cb811ed3d251aea01ec8576dd4eef9a2ca3c94e
bkh76 <55768460+bkh76@users.noreply.github.com>
2020-02-27 17:42:25 -0700

    Actually updated Visitor.java
    Actual update

--- nap/ast/Visitor.java
+++ nap/ast/Visitor.java
@@ -1,15 +1,32 @@
 package ast;
 
 public interface Visitor<T> {
-    public T visit(ExpBinop expression);
-    public T visit(ExpEqualityop expression);
-    public T visit(ExpLogicalop expression);
-    public T visit(ExpNegop expression);
-    public T visit(ExpRelationop expression);
-    public T visit(ExpInt number);
+    //expressions
+    public T visit(ExprArray expression);
+    public T visit(ExprArrayInit expression);
+    public T visit(ExprBinop expression);
+    public T visit(ExprBoolean expression);
+    public T visit(ExprChar expression);
+    public T visit(ExprEquality expression);
     public T visit(ExprIdentifier variable);
+    public T visit(ExprInequality expression);
+    public T visit(ExprInt number);
+    public T visit(ExprLogical expression);
+    public T visit(ExprNegation expression);
+    public T visit(ExprString expression);
+    
+    //instructions
+    public T visit(Assign instruction);
+    public T visit(Conditional instruction);
+    public T visit(ElseCond instruction);
+    public T visit(ElseIfCond instruction);
+    public T visit(IfCond instruction);
+    public T visit(New instruction);
     public T visit(Print instruction);
     public T visit(Read instruction);
-    public T visit(Assign instruction);
+    public T visit(Return instruction);
+    public T visit(WhileCond instruction);
+        
+    //program
     public T visit(Program program);
 }
